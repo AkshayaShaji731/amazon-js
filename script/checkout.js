a464d9e7db7aec82a75f1ae2d6ed558a5dd9a125
Akshaya Shaji <akshayashaji66@gmail.com>
2025-03-20 10:30:20 +0530

    delete products from cat when clicking delete

--- script/checkout.js
+++ script/checkout.js
@@ -1,5 +1,5 @@
 
-import {cart} from "../data/cart.js";
+import {cart,removeFromCarrt} from "../data/cart.js";
 import {products} from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -39,7 +39,7 @@ cart.forEach((cartitem)=>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${matchingProduct.id}>
                     Delete
                   </span>
                 </div>
@@ -95,4 +95,12 @@ cart.forEach((cartitem)=>{
 });
 
 document.querySelector(".js-order-summary").innerHTML=cartSummaryHTML
-console.log(cartSummaryHTML);
+
+document.querySelectorAll(".js-delete-link").forEach((link)=>{
+  link.addEventListener("click",()=>{
+    const productId=link.dataset.productId
+    removeFromCarrt(productId)
+
+  })
+})
+
