b0162756d7f135c2b5d23c2476b1c306406af9d9
Akshaya Shaji <akshayashaji66@gmail.com>
2025-03-20 10:44:27 +0530

    delete button from HTML when click delete btn

--- script/checkout.js
+++ script/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartitem)=>{
     });
     cartSummaryHTML+=
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${productId}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -100,7 +100,9 @@ document.querySelectorAll(".js-delete-link").forEach((link)=>{
   link.addEventListener("click",()=>{
     const productId=link.dataset.productId
     removeFromCarrt(productId)
-
+    
+    const container=document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove()
   })
 })
 
